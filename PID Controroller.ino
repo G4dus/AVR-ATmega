22dffdfcf37c6b84b1006a2cb2f32306593543c0
G4dus <32588510+G4dus@users.noreply.github.com>
2018-11-06 11:11:39 +0100

    Update PID Controroller.ino

--- PID Controroller.ino
+++ PID Controroller.ino
@@ -1,16 +1,15 @@
-#include <FastPID.h>
+//
+//Download library bellow
 #include <max6675.h>
+#include <PID_v1.h>
 
-
-#define PIN_OUTPUT    3
-
-float Kp=20, Ki=1, Kd=2, Hz=600;
-
-int output_bits = 8;
-bool output_signed = false;
-FastPID myPID(Kp, Ki, Kd, Hz, output_bits, output_signed);
-
+//PID_1
+double Setpoint = 216;                                      //define Setpoint 1 of 2 216..
 double Input_1;
+double Output_1;                          
+double Kp_1=2.8, Ki_1=0.01, Kd_1=150;                                         //Set Kp, Ki, Kd            
+PID PID_1(&Input_1, &Output_1, &Setpoint, Kp_1, Ki_1, Kd_1, DIRECT); 
+
 
 //thermocople max6675 config
 int thermoSO = 12;
@@ -22,37 +21,44 @@ MAX6675 thermocouple(thermoCLK, thermoCS, thermoSO);
 
 
 //motor setup
-
 int analogInput = A0;
-int Motor_Speed = 0;
-int Motor_PWM = 4;
+int Motor_Speed =0;
+int Motor_PWM = 5;
+
+//puller motor
+int StepPin = 6;
+int analogInputStepSpeed = A1;
+int StepSpeed = 5;
 
 
 //interval
 unsigned long previousMillis = 0; 
-const long interval = 280;   
+const long interval_1 = 250;  
+unsigned long previousMillis2 = 0; 
+const long interval_2 = 5;    
 
-unsigned long previousMillis_2 = 0; 
-const long interval_2 = 250;                                                   //Set interval time [ms]
+
   
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(1, INPUT_PULLUP); //#Setpoint up
-  pinMode(0, INPUT_PULLUP); //#Setpoint down
-  pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
-  pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
-  
+  Serial.begin(115200);
+
   pinMode(Motor_PWM, OUTPUT); //Motor speed PWM
+
+  pinMode(StepPin, OUTPUT); //Puller step stepper motor
   
   pinMode(thermoCS, OUTPUT);
   pinMode(thermoSO, INPUT);
   pinMode(thermoCLK, OUTPUT);
+  pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
+  pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
 
-
-
+// double Setpoint = 216;                                                //defile setpoint 2 of 2
   Input_1 = thermocouple.readCelsius(); 
-
+  if (Input_1 == 0)
+  {Input_1 = 999;}
+  PID_1.SetMode(AUTOMATIC);
+  PID_1.SetTunings(Kp_1, Ki_1, Kd_1);
 
  delay(500); //load Max6675 config please wait...
 
@@ -60,76 +66,43 @@ void setup()
 
 void loop()
      {
-      int Setpoint =150;
-//delay(240);
-      unsigned long currentMillis = millis();
       
-    if (1==0)//(digitalRead(1) == LOW)
-       {
-       Setpoint = Setpoint + 1;
-       delay(250);
-       } 
-       
-    if (1==0)  // (digitalRead(0) == LOW)
-       {
-       Setpoint = Setpoint -1;
-       delay(250);
-       }     
+      unsigned long currentMillis = millis();
+
+      PID_1.Compute();                        //Call and calculate PID 
+      analogWrite(3,Output_1);                  //output PID controler
+
+  
+      if (currentMillis - previousMillis >= interval_1) //cyclic interrupts_1
+         {
+           previousMillis = currentMillis;
+           Input_1 = thermocouple.readCelsius(); //You have to call readCelsius in intervals otherwise crash MAX6675
+           
+           if (Input_1 == 0)
+              {
+                Input_1 = 999;
+              }
+         }
 
 
-  int setpoint = 150;
-  int feedback = Input_1;
-  uint32_t before, after;
-  before = micros();
-  uint8_t output = myPID.step(setpoint, feedback);
-  after = micros();
-analogWrite(PIN_OUTPUT, output);
+      if (currentMillis - previousMillis2 >= interval_2) //cyclic interrupts_2
+         {
+           previousMillis2 = currentMillis;
+          digitalWrite(StepPin, HIGH);
+         }
+         
+         digitalWrite(StepPin, LOW);
+                            
 
 
-
-
-
-
-  if (currentMillis - previousMillis >= interval) //cyclic interrupts
-     {
-      previousMillis = currentMillis;
-      Input_1 = thermocouple.readCelsius(); //You have to call readCelsius in intervals otherwise crash MAX6675
-     }
-
-
-
-      if (currentMillis - previousMillis_2 >= interval_2) //cyclic interrupts
-     {
-      previousMillis_2 = currentMillis;
-     
-//  Serial.print(Input_1);
-//  Serial.print("    ");
-//  Serial.println(Setpoint);
-
-
-  Serial.print("runtime: "); 
-  Serial.print(after - before);
-  Serial.print(" sp: "); 
-  Serial.print(setpoint); 
-  Serial.print(" fb: "); 
-  Serial.print(feedback);
-  Serial.print(" out: ");
-  Serial.println(output);
-  delay(100);
-     }
- 
- //logs
+  
+//  Serial.print(" ");
+  Serial.println(Input_1);
 
 
 
-
-/*
-*
-* PWM Motor control below
-*
-*/
-
- int Motor_Speed = analogRead(analogInput);
+ //PWM Motor control below
+ Motor_Speed = analogRead(analogInput);
  analogWrite(Motor_PWM, Motor_Speed /4);
 
 }
@@ -149,16 +122,16 @@ analogWrite(PIN_OUTPUT, output);
 *       A5 []
 * ----------------
 * Digital:       
-*       0  [x]  Setpoint down
-*       1  [x]  Setpoint up
+*       0  []  
+*       1  []
 *       2  []
 *       3  [x]  PID PWM output 0-255
-*       4  [x]  Motor PWM output 0-255
-*       5  []
-*       6  []
+*       4  []  
+*       5  [x]  Motor PWM output 0-255
+*       6  [x]  Puller motor step
 *       7  []
-*       8  [x]  MAX6675 GND [remove if you need pin] 
-*       9  [x]  MAX6675 VCC [remove if you need pin]
+*       8  [x]  MAX6675 GND [remove it if you need pin] 
+*       9  [x]  MAX6675 VCC [remove it if you need pin]
 *       10 [x]  MAX6675 CLK
 *       11 [x]  MAX6675 CS
 *       12 [x]  MAX6675 SO
